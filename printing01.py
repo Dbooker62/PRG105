21efaca947784616b38d8ce8f330a2c036c6c448
Dbooker62 <123719363+Dbooker62@users.noreply.github.com>
2023-02-07 13:09:01 -0600

    Add files via upload

---
+++ printing01.py
@@ -0,0 +1,52 @@
+# Printing this poem
+
+print("It's")
+print("cold outside")
+print("I don't want to go")
+print("outdoors and play.")
+print("But Mum says")
+print("I have to")
+print("anyway.")
+
+print("It's starting to snow)")
+print("and I'm going to freeze-")
+print("I hate playing outside on days like these")
+print("But wait a sec. I've had the")
+print("most amazing, brilliant idea! ")
+print("I'll cover myself up")
+print("with snow and I'll")
+print("hide in here!")
+
+
+# get new words from the person
+person = input("you")
+verb = input("leave")
+verb = input("sister")
+place = input("school")
+noun = input("balls")
+adverb = input("crazy")
+place = input("yard")
+noun = input("afternoon")
+
+# substitute the words into the poem
+print(f"{pronoun} don't want to go")
+
+
+
+
+#  This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
